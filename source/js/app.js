3ae8555681c54f2c77f24c3c2b203d8786c8fd08
Sokol V <1470715@gmail.com>
2018-07-16 12:24:43 +0300

    if/else

--- source/js/app.js
+++ source/js/app.js
@@ -24,9 +24,40 @@ function g(){
 console.log(f(), x);
 console.log(g(), y);*/
 
-var a = [1,2,3,4,5,6,7,8,9];
+/*function typeVar(x){
+	switch(typeof x){
+		case 'number': console.log('This is number')
+			break;
+		case 'string': console.log('This is string')
+			break;
+		default: console.log('other type')
+			break;
+	}
+}
+
+typeVar()*/
+/*var a = [1,2,3,4,5,6,7,8];
+var b = [];
+
+for(i = 0; i < a.length; i++){
+	console.log(a[i]*2);
+}
+console.log(a);*/
+
+/*var a = {a: 1, b: 2, c: 3};
+
+for(var p in a){
+	console.log(p);
+}
+*/
+
+var a = [1,2,3,4,5,'asdf',6,7,'asdf',8,'asdf',9];
+/*var i;
+var total = 0
+
+for(i = 0; i < a.length; i++){
+	console.log(a[i]);
+}*/
 
 delete a[0];
-
 console.log(a);
-alert('hello')
