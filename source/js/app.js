98e641a0b428dbb4ba4c2a2cd09d2a6ecd93d8a8
Sokol V <1470715@gmail.com>
2018-07-26 12:06:42 +0300

    test task finish

--- source/js/app.js
+++ source/js/app.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
 	$('.banner__close').on('click', function(){
 		$('.banner').slideUp();
-	})
+	});
+	$('.menu__btn').on('click',function(e){
+		e.preventDefault;
+		$(this).toggleClass('menu__btn--active');
+		$('.menu__mobile').toggleClass('menu__mobile--active');
+	});
+	$('.search__icon').on('click', function(e){
+		e.preventDefault;
+		$('.search__input').toggleClass('search__input--active');
+	});
   svg4everybody();
 });
