5c7f7d2df439f285b0fea25a1e4af6b14d11b9b4
Sokol V <1470715@gmail.com>
2018-07-31 11:52:59 +0300

    form methods

--- source/js/app.js
+++ source/js/app.js
@@ -15,21 +15,42 @@ $(document).ready(function() {
 	$('.inc').on('click', function(e){
 		e.preventDefault();
 		for(var i = 0; i < document.images.length; i++){
-			document.images[i].style.width = document.images[i].width + 10 + 'px';
+			document.images[i].style.width = document.images[i].width + 5 + 'px';
 		}
 	})
-	$('.deg').on('click', function(e){
+	$('.dec').on('click', function(e){
 		e.preventDefault();
 		for(var i = 0; i < document.images.length; i++){
-			document.images[i].style.width = document.images[i].width - 10 + 'px';
+			document.images[i].style.width = document.images[i].width - 5 + 'px';
 		}
 	})
+	$('.toTop').on('click',function(e){
+		e.preventDefault();
+		window.scrollTo(0,0);
+	});
 
-var form = document.getElementsByClassName('form');
+/*var shipping_method
+var form = document.forms.shipping;
+var elem = form.elements;
+for(i = 0; i < elem.length; i++){
+	if(elem[i].checked) {shipping_method = elem[i].value}
+}
+console.log(shipping_method);
+*/
+/*var formName;
+var form = document.forms.fio;
+var elem = form.elements;
+for(i = 0; i < elem.length; i++){
+	console.log(elem[i].value)
+}*/
 
-var para = document.getElementsByClassName('text');
-$('.text').append(' lololol');
-
-
+function getSelectedText(){
+	if(window.getSelection){
+		return window.getSelection().toString();
+	} else if(document.selection){
+		return document.selection.createRange().text;
+	}
+}
+getSelectedText()
   svg4everybody();
 });
