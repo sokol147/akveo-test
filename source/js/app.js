705263ade0ad4b005e09e8a4140843bc2a0e7506
Sokol V <1470715@gmail.com>
2018-08-02 12:01:24 +0300

    fix gulp 4 bug

--- source/js/app.js
+++ source/js/app.js
@@ -51,6 +51,66 @@ function getSelectedText(){
 		return document.selection.createRange().text;
 	}
 }
-getSelectedText()
+getSelectedText();
+
+
+function inputValidation(){
+  var inputelts = document.getElementsByTagName('input');
+
+  for(var i = 0; i < inputelts.length; i++){
+    var elt = inputelts[i];
+
+    if(elt.type != 'text' || !elt.getAttribute('data-allowed-chars')){
+      continue;
+    }
+
+    if(elt.addEventListener){
+      elt.addEventListener('keypress', filter, false);
+      elt.addEventListener('taxtInput', filter, false);
+      elt.addEventListener('textinput', filter, false);
+    } else {
+      elt.attachEvent('onkeypress', filter);
+    }
+  }
+
+  function filter(event){
+    var e = event || window.event;
+    var target = e.target || e.srcElement;
+    var text = null;
+
+    if(e.type === 'textinput' || e.type === 'textInput') text = e.data;
+    else{
+      var code = e.charCode || e.keyCode;
+
+      if(code < 32 || e.charCode == 0 || e.ctrlKey || e.altKey) return;
+
+      var text = String.fromCharCode(code);
+    }
+    var allowed = target.getAttribute('data-allowed-chars');
+    var messageid = target.getAttribute('data-messageid');
+    if(messageid){
+      var messageElement = document.getElementById(messageid);
+    }
+    for(var i = 0; i < text.length; i++){
+      var c = text.charAt(i);
+      if(allowed.indexOf(c) == -1){
+        if(messageElement) messageElement.style.visibility = 'visible';
+
+        if(e.preventDefault) e.preventDefault();
+
+        if(e.returnValue) e.returnValue = false;
+        return false;
+      }
+    }
+    if(messageElement) messageElement.style.visibility = 'hidden';
+  }
+}
+inputValidation();
+
+var request = new XMLHttpRequest();
+
+request.open('GET','data.cvs');
+
+
   svg4everybody();
 });
