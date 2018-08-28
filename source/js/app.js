a9b6b34b480c493689338d72e937bdb774f19122
Sokol V <1470715@gmail.com>
2018-08-28 11:36:39 +0300

    scope

--- source/js/app.js
+++ source/js/app.js
@@ -85,10 +85,43 @@ getLocalDay(now);*/
 }
 console.log(getSecondsToday());*/
 
-var date = new Date(2014,0,30);
+/*var date = new Date(2014,0,30);
 
 function formatDate(date){
-	console.log(date.toLocaleString('ru', {day: 'numeric', month: 'numeric', year: 'numeric'}))
+	console.log(date
+		.toLocaleString('ru', {day: 'numeric', month: 'numeric', year: 'numeric'}))
 }
 
-formatDate(date);
+formatDate(date);*/
+
+/*function sum(a){
+	return function(b){
+		return a + b;
+	};
+}
+
+console.log(sum(1)(2));*/
+
+function makeBuffer(){
+	var currentBuffer = '';
+
+	function buffer(piece){
+		if(arguments.length == 0){
+			return currentBuffer;
+		}
+		currentBuffer += piece;
+	}
+	buffer.clean = function(){
+		currentBuffer = '';
+	}
+	return buffer;
+}
+
+var buffer = makeBuffer();
+
+buffer('Замыкания');
+buffer(' Использовать');
+buffer(' Нужно!');
+buffer.clean();
+
+console.log( buffer() );
