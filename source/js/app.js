9f2742be9e543ec8f9751c37230a61c325885511
Sokol V <1470715@gmail.com>
2018-09-13 10:41:26 +0300

    add google maps api

--- source/js/app.js
+++ source/js/app.js
@@ -11,3 +11,26 @@ menuBtn.onclick = function(event){
   menuList.classList.toggle('open');
 
 };
+
+
+
+function initMap(){
+  var uluru = {lat: 37.876240, lng: -122.250098};
+
+  var map = new google.maps.Map(
+    document.getElementById('map__wrapper'), {zoom: 15, center: uluru}
+  );
+
+  var marker = new google.maps.Marker({
+    position: uluru, 
+    map: map,
+    icon: {
+      url: 'templates/images/map-icon.png',
+      scaledSize: new google.maps.Size(64,64)
+    }
+  });
+
+}
+
+window.addEventListener('load',initMap);
+
